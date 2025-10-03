b40d98d6af99de237dda3282dd630a1d9d09f8cb
ali27-dev <khanali3229@gmail.com>
2025-10-03 11:28:13 +0500

    I'm insert all images and logos to new folder images then i'm bulid next  awards section.

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="container p-5">
+    <div className="container p-5 mb-5">
       <div className="row text-center">
-        <img src="Assets/homeHero.png" alt="" className="mb-5" />
+        <img src="Assets/images/homeHero.png" alt="" className="mb-5" />
         <h1 className="mt-5">Invest in everything</h1>
         <p>
           Online platform to invest in stocks, derivatives, mutual funds, and
           more
         </p>
         <button
-          className="p-2 btn btn-primary fs-5"
+          className="p-2 btn btn-primary fs-5 mb-5"
           style={{ width: "20%", margin: "0 auto" }}
         >
           SignUp
