cbe9293d428b12d771b7b29b7c8ff20f53321c5b
ali27-dev <khanali3229@gmail.com>
2025-10-04 21:26:04 +0500

    The about section is completed

--- frontend/src/landing_page/about/AboutPage.js
+++ frontend/src/landing_page/about/AboutPage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar";
 import Hero from "./Hero";
-import Pricing from "../home/Pricing";
+import Team from "./Team";
 import Footer from "../Footer";
 
 function AboutPage() {
   return (
     <>
       <Hero />
-      <Pricing />
+      <Team />
     </>
   );
 }
