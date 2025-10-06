04c4dd276edc57a65df7d63b0359a2e024645582
ali27-dev <khanali3229@gmail.com>
2025-10-06 12:05:51 +0500

    In Support component, The CreateTicket sectio0n is complete and move next section.

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,7 +1,358 @@
 import React from "react";
 
 function CreateTicket() {
-  return <h1>Create Tickets</h1>;
+  return (
+    <div className="container">
+      <div className="row mt-5 p-5">
+        <h1 className="">To create a ticket, select a relevant topic</h1>
+      </div>
+      {/* Tickets-Link-section-1-start */}
+      <div className="row  p-5">
+        {/* colum-1 */}
+        <div className="col-4  mb-5">
+          <h4 className="text-muted mb-5">
+            <i class="ri-add-circle-fill"></i> Account Opening
+          </h4>
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Online Account Opening
+          </a>
+
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Offline Account Opening
+          </a>
+
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Company, Partnership and HUF Account
+          </a>
+
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Opening
+          </a>
+
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            NRI Account Opening
+          </a>
+
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Charges at Zerodha
+          </a>
+
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Zerodha IDFC FIRST Bank 3-in-1 Account
+          </a>
+
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Getting Started
+          </a>
+        </div>
+
+        {/* colum-2 */}
+        <div className="col-4">
+          <h4 className="text-muted mb-5">
+            <i class="ri-account-pin-circle-fill"></i> Your Zerodha Account
+          </h4>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Login Credentails
+          </a>
+
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Account Modification and Segment Adition
+          </a>
+
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            DP ID and bank details
+          </a>
+
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Your Profile
+          </a>
+
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Transfer and conversion of shares
+          </a>
+        </div>
+        {/* colum-3 */}
+        <div className="col-4 ">
+          <h4 className="text-muted mb-5">
+            <i class="ri-line-chart-fill"></i> Your Zerodha Account
+          </h4>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Margin/leverage, Product and Order types
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Kite Web and Mobile
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Tranding FAQs
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Corporate Actions
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Sentinel
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Kite API
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Pi and other platforms
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Stockerports+
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            GTT
+          </a>
+        </div>
+      </div>
+      {/* Tickets-Link-section-1-end */}
+
+      {/* Tickets-Link-section-2-start */}
+      <div className="row  p-5">
+        {/* colum-1 */}
+        <div className="col-4  mb-5">
+          <h4 className="text-muted mb-5">
+            <i class="ri-bank-card-2-line"></i> Funds
+          </h4>
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Adding Funds
+          </a>
+
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Fund Withdrawal
+          </a>
+
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            eMandates
+          </a>
+
+          <a
+            href="#"
+            className="footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Adding Bank Accounts
+          </a>
+        </div>
+
+        {/* colum-2 */}
+        <div className="col-4">
+          <h4 className="text-muted mb-5">
+            <i class="ri-restart-line"></i> Console
+          </h4>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Reports
+          </a>
+
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Ledger
+          </a>
+
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Protfolio
+          </a>
+
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            60 Day Challenge
+          </a>
+
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            IPO
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Referral Program
+          </a>
+        </div>
+        {/* colum-3 */}
+        <div className="col-4">
+          <h4 className="text-muted mb-5">
+            <i class="ri-coins-line"></i> Coin
+          </h4>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Understand Muthual Funds
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            About Coin
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Buying and Selling through Coin
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Starting an SIP
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Managing your Portfolio
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Coin App
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Moving to Coin
+          </a>
+          <a
+            href="#"
+            className=" footer-link"
+            style={{ textDecoration: "none" }}
+          >
+            Government Securities
+          </a>
+        </div>
+      </div>
+      {/* Tickets-Link-section-2-end */}
+    </div>
+  );
 }
 
 export default CreateTicket;
--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -9,7 +9,6 @@ function SupportPage() {
     <>
       <Hero />
       <CreateTicket />
-      <OpenAccount />
     </>
   );
 }
