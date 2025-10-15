5a0990e93b0109597a4422d9acada5801d584239
ali27-dev <khanali3229@gmail.com>
2025-10-15 09:41:33 +0500

    First of all I'm link backend with mondoDB and then i'm creates Schemas and Models

---
+++ backend/model/HoldingsModel.js
@@ -0,0 +1,6 @@
+const { model } = require("mongoose");
+const { HoldingsSchema } = require("../schemas/HoldingsShema");
+
+const HoldingsModel = new model("holding", HoldingsSchema);
+
+module.exports = { HoldingsModel };
