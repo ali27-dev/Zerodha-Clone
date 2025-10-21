ef95badb8e1e5d9cacb7f0d803ce59bdb1740a32
ali27-dev <khanali3229@gmail.com>
2025-10-21 20:32:14 +0500

    After the implementing signup logic,I'm also implement for login and its also working fine.

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -2,6 +2,7 @@ const User = require("../model/UsersModel");
 const { createSecretToken } = require("../SecretToken");
 const bcrypt = require("bcryptjs");
 
+////////Signup-Logic ///////
 module.exports.Signup = async (req, res, next) => {
   try {
     const { email, password, username, createdAt } = req.body;
@@ -18,6 +19,35 @@ module.exports.Signup = async (req, res, next) => {
     res
       .status(201)
       .json({ message: "User signed in successfully", success: true, user });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//////// Login-logic ///////
+module.exports.Login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User logged in successfully", success: true });
     next();
   } catch (error) {
     console.error(error);
--- backend/Routes/AuthRoute.js
+++ backend/Routes/AuthRoute.js
@@ -2,7 +2,7 @@ const { Signup, Login } = require("../Controllers/AuthController");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
-// router.post("/login", Login);
+router.post("/login", Login);
 // router.get("/signup", (req, res) => {
 //   res.send("form");
 // });
