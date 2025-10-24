9bce00b80e46df37e89e42c0a789ae3b0aa443c3
ali27-dev <khanali3229@gmail.com>
2025-10-24 21:17:11 +0500

    Lastly we also touch the testing,I'm create test suite for Hero.

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -4,7 +4,11 @@ function Hero() {
   return (
     <div className="container p-5 mb-5">
       <div className="row text-center">
-        <img src="Assets/images/homeHero.png" alt="" className="mb-5" />
+        <img
+          src="Assets/images/homeHero.png"
+          alt="Hero Image"
+          className="mb-5"
+        />
         <h1 className="mt-5">Invest in everything</h1>
         <p>
           Online platform to invest in stocks, derivatives, mutual funds, and
