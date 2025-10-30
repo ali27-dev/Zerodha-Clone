d7a84259584ad4d2eb0d92d45152aca725bdab9c
ali27-dev <khanali3229@gmail.com>
2025-10-30 20:29:24 +0500

    i'm clean the code some places i'm comments large code, So i'm remove that all code and add gitignore file

--- dashboard/src/components/SignupForm.js
+++ dashboard/src/components/SignupForm.js
@@ -12,6 +12,7 @@ const Signup = () => {
     username: "",
   });
   const { email, password, username } = inputValue;
+
   const handleOnChange = (e) => {
     const { name, value } = e.target;
     setInputValue({
@@ -72,9 +73,7 @@ const Signup = () => {
           placeholder="Enter your email"
           onChange={handleOnChange}
         />
-        {/* </div> */}
-        {/* <div> */}
-        {/* <label htmlFor="email">Username</label> */}
+
         <input
           type="text"
           name="username"
@@ -82,9 +81,7 @@ const Signup = () => {
           placeholder="Enter your username"
           onChange={handleOnChange}
         />
-        {/* </div> */}
-        {/* <div> */}
-        {/* <label htmlFor="password">Password</label> */}
+
         <input
           type="password"
           name="password"
@@ -92,7 +89,7 @@ const Signup = () => {
           placeholder="Enter your password"
           onChange={handleOnChange}
         />
-        {/* </div> */}
+
         <button type="submit">Submit</button>
         <span>
           Already have an account? <Link to={"/login"}>Login</Link>
@@ -104,70 +101,3 @@ const Signup = () => {
 };
 
 export default Signup;
-/* OLD-Signup-Logic */
-// import React, { useState } from "react";
-// import axios from "axios";
-// import "./css/AuthForm.css";
-
-// const SignupForm = () => {
-//   const [formData, setFormData] = useState({
-//     name: "",
-//     email: "",
-//     password: "",
-//   });
-
-//   const handleChange = (e) =>
-//     setFormData({ ...formData, [e.target.name]: e.target.value });
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const res = await axios.post("http://localhost:4000/signup", formData, {
-//         withCredentials: true,
-//       });
-//       alert("Signup successful!");
-//       console.log(res.data);
-//     } catch (err) {
-//       console.error(err);
-//       alert("Signup failed!");
-//     }
-//   };
-
-//   return (
-//     <div className="auth-container">
-//       <h2>Create Account</h2>
-//       <form className="auth-form" onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           name="name"
-//           placeholder="Full Name"
-//           value={formData.name}
-//           onChange={handleChange}
-//           required
-//         />
-
-//         <input
-//           type="email"
-//           name="email"
-//           placeholder="Email Address"
-//           value={formData.email}
-//           onChange={handleChange}
-//           required
-//         />
-
-//         <input
-//           type="password"
-//           name="password"
-//           placeholder="Create Password"
-//           value={formData.password}
-//           onChange={handleChange}
-//           required
-//         />
-
-//         <button type="submit">Sign Up</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default SignupForm;
